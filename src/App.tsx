81f662d0f138503da16f2141309fba4647e05ac8
Viveka Barneaud <viveka.barneaud@aaaic.com>
2022-12-19 14:04:59 +0100

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,9 @@ import styles from './App.module.css';
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 };

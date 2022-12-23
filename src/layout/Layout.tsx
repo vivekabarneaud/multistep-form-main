9eae38e2d7ca9c432ff5b4e626a7c29e9a27f741
Viveka Barneaud <viveka.barneaud@aaaic.com>
2022-12-23 15:03:37 +0100

    souviens plus

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -10,12 +10,16 @@ const Layout: ParentComponent = (props) => {
 
     return (<>
         <Show when={screenW() > 576} fallback={
-            <div class="flex flex-col items-center justify-start w-full h-full">
+            <div class="flex flex-col w-full min-h-screen">
                 <div class="flex px-24 justify-between items-center w-full">
                     <A href="/your-info" class="step-btn" activeClass="step-btn-active">1</A>
                     <A href="/select-plan" class="step-btn" activeClass="step-btn-active">2</A>
                     <A href="/add-ons" class="step-btn" activeClass="step-btn-active">3</A>
                     <A href="/summary" class="step-btn" activeClass="step-btn-active">4</A>
+                </div>
+
+                <div class="flex flex-col rounded-lg bg-white shadow-lg pt-20 px-10">
+                    djfksjf
                 </div>
             </div>
         }>big screen

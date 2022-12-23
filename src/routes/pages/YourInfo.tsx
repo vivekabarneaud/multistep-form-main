e399b995706f76866be1337a0482d2ebffbe5d6f
Viveka Barneaud <viveka.barneaud@aaaic.com>
2022-12-23 16:54:26 +0100

    created class for text inputs

--- src/routes/pages/YourInfo.tsx
+++ src/routes/pages/YourInfo.tsx
@@ -8,11 +8,11 @@ const YourInfo: Component = () => {
         <p class="max-w-xl text-gray-400 mb-8">Please provide your name, email address, and phone number.</p>
         <form class="flex flex-col gap-y-1">
             <label for="name" class="text-sm font-medium text-[#0E2E60]">Name</label>
-            <input type="text" placeholder="Enter your name here..." class="px-3 py-2 border border-gray-200 rounded-lg font-medium text-[#345172] placeholder:font-normal"/>
+            <input type="text" placeholder="Enter your name here..." class="text-input"/>
             <label for="email" class="text-sm font-medium text-[#0E2E60]">Email address</label>
-            <input type="text" placeholder="Enter your email address..." class="px-3 py-2 border border-gray-200 rounded-lg font-medium text-[#345172] placeholder:font-normal"/>
+            <input type="text" placeholder="Enter your email address..." class="text-input"/>
             <label for="phone" class="text-sm font-medium text-[#0E2E60]">Phone number</label>
-            <input type="text" placeholder="e.g. +1 234 567 890" required class="px-3 py-2 border border-gray-200 rounded-lg font-medium text-[#345172] placeholder:font-normal required:border-red-500"/>
+            <input type="text" placeholder="e.g. +1 234 567 890" required class="text-input required:outline-red-500"/>
 
             <div class="flex justify-end pt-36">
                 <button type="submit" class="px-3 py-2 rounded bg-[#042859] hover:bg-[#174A8B] text-white">Next step</button>

41e03904b1751433e9e6e4797803f85e55873487
Viveka Barneaud <viveka.barneaud@aaaic.com>
2023-01-07 16:21:28 +0100

    tried with /dist/ instead of /public/ but maybe its disgusting

--- src/routes/pages/ThankYou.tsx
+++ src/routes/pages/ThankYou.tsx
@@ -2,7 +2,7 @@ import {Component} from "solid-js";
 
 const ThankYou: Component = () => {
     return (<div class="flex flex-col h-full items-center justify-center gap-y-3 pb-20 md:pb-8">
-        <img src="/public/assets/images/icon-thank-you.svg" class="w-16 h-16 mb-2"/>
+        <img src="/dist/assets/images/icon-thank-you.svg" class="w-16 h-16 mb-2"/>
         <h2 class="text-2xl font-bold text-[#0E2E60]">Thank you!</h2>
         <p class="text-sm max-w-xl px-8 text-gray-400 text-center leading-5">Thanks for confirming your subscription!
             We hope you have fun using our platform.
